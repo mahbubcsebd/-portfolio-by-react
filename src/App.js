df7ce60d6e977c7dc7cf5c1f0baab6b48ca11450
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2022-07-26 03:21:15 +0600

    deploy

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import './App.css';
 //import { v4 as uuidv4 } from 'uuid';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import Error from './pages/Error';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -13,9 +10,6 @@ function App() {
     <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
